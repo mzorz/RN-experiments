6ae8cb32a2a770c745437967033b1e858e6cbe53
Mario Zorz <mariozorz@gmail.com>
2018-03-21 14:58:20 -0300

    setState makes the component re-render, this commit is a reminder to that. This will change the style height value according to the lineCount value as signaled by the underlying AztecTextView RN component implementation

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ class HelloWorld extends React.Component {
 
 constructor (props) {
     super(props);
-    this.state = { text: 'ESTO ES UNA PRUEBA'};
-    //this._onChange = this._onChange.bind(this);
+    this.state = { text: 'ESTO ES UNA PRUEBA', lineCount: 0};
+    this._onChange = this._onChange.bind(this);
+    this.state.lineCount = 0
+    console.log("setting constructor value: " + this.state.lineCount);
   }
 
   _onChange(event: Event) {
@@ -18,35 +20,56 @@ constructor (props) {
     //   return;
     // }
     // this.props.onChangeMessage(event.nativeEvent.message);
+    console.log('_onChange current state: ' + JSON.stringify(this.state));
+    //this.state.lineCount = event.nativeEvent.lineCount;
+    // this.setState(() => {
+    //             console.log('setting state');
+    //             return { text: 'ESTO ES UNA PRUEBA', lineCount: event.nativeEvent.lineCount};
+    //         });
+
+    this.setState({ text: 'ESTO ES UNA PRUEBA', lineCount: event.nativeEvent.lineCount});
+
+    console.log("setting this state value: " + this.state.lineCount);
+  }
+
+  _aztecwrapperviewStyle() {
+    console.log("the calculation should give this: "+ (this.state.lineCount+1) * 40);
+    return {
+      flex: 1,
+      justifyContent: 'center',
+      borderColor: 'gray', 
+      borderWidth: 2,
+      height: (this.state.lineCount+1) * 40
+      //height: 200
+    }
   }
 
   render() {
+    console.log("render called")
     return (
       <View style={styles.container}>
-        <AztecTextRNView
-         style={styles.hello}
-         color = {'black'}
-         text = {'AztecTextRNView hello thi is a atest from js'}
-         maxImagesWidth = {300}
-         editable = {true}
-         autoGrow = {true}
-         multiline = {true}
-         onChange={this._onChange}
-         onLineCountChange = {true}
-         //onChange={(text) => this.setState({text})}
-        />
-        <TextInput
-          style={{borderColor: 'gray', borderWidth: 1}}
-          value={this.state.text}
-          editable = {true}
-          multiline = {true}
-          autoGrow = {false}
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={styles.hello}>
-          TEXTO: {JSON.stringify(this.state.text)}
-        </Text>
-      </View>
+        <View style={this._aztecwrapperviewStyle()}>
+          <AztecTextRNView
+           style={styles.hello}
+           color = {'black'}
+           text = {'AztecTextRNView hello thi is a atest from js\n new line here let us see'}
+           onChange={this._onChange}
+           onLineCountChange = {true}
+           //onChange={(text) => this.setState({text})}
+          />
+          <TextInput
+            style={{borderColor: 'gray', borderWidth: 1}}
+            value={this.state.text}
+            editable = {true}
+            multiline = {true}
+            autoGrow = {false}
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Text style={styles.hello}>
+            TEXTO: {JSON.stringify(this.state)}
+          </Text>
+        </View>
+       </View>
     );
   }
 }
